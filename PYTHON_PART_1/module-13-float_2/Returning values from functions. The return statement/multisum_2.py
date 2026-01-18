fa55dafa9ebffc496c0c5235a7831ffd3d77b105
vshishanovart-star <vshishanov.art@gmail.com>
2026-01-18 21:02:04 +0000

    Add file multisum_2

---
+++ PYTHON_PART_1/module-13-float_2/Returning values from functions. The return statement/multisum_2.py
@@ -0,0 +1,10 @@
+a = 100
+b = 200
+c = 300
+if c > b:
+    if b > a:
+        print("c is greater than b and b is greater than a")
+    else:
+        print("c is greater than b but b is not greater than a")
+else:
+    print("c is not greater than b")
